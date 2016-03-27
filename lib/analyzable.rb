79d3e510dbae227f28c6d7579dc1a2ad7b75bfc3
jboludae <jboludae@gmail.com>
2016-03-27 23:39:53 +0200

    anslyzable test for average price is ok

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,7 @@
 module Analyzable
-  # Your code goes here!
+  def average_price(arr)
+    prices = arr.map {|product| product.price}
+    price = prices.inject(0) {|sum, val| sum.to_f + val.to_f} / arr.length
+    return price.round(2)
+  end
 end
